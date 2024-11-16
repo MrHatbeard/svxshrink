bbdd000fb5732cde94c5fc2f1d1d919d4fa06f19
MrHatbeard <75539987+MrHatbeard@users.noreply.github.com>
2024-11-16 22:39:45 +0000

    Update install-pishrink.sh

--- install-pishrink.sh
+++ install-pishrink.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y wget parted gzip pigz xz-utils udev e2fsprogs
 wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
-chmod +x pishrink.sh
-chmod +x shrink
-sudo mv pishrink.sh /usr/local/bin
-sudo mv shrink /usr/local/bin
+chmod +xv pishrink.sh
+chmod +xv shrink
+sudo mv -v pishrink.sh /usr/local/bin
+sudo mv -v shrink /usr/local/bin
